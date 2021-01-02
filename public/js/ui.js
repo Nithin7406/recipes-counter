c57bf35855cf9559f167e909b95b570694a31305
nithin7316 <nithinkumar6330.nk@gmail.com>
2021-01-02 13:19:21 +0530

    Initial setup

---
+++ public/js/ui.js
@@ -0,0 +1,28 @@
+const recipes = document.querySelector(".recipes");
+document.addEventListener("DOMContentLoaded", function () {
+  // nav menu
+  const menus = document.querySelector(".side-menu");
+  M.Sidenav.init(menus, { edge: "right" });
+  // add recipe form
+  const forms = document.querySelector(".side-form");
+  M.Sidenav.init(forms, { edge: "left" });
+});
+//render recipe data
+const renderRecipe = (data, id) => {
+  const html = `<div class="card-panel recipe white row" data-id="${id}">
+  <img src="/img/dish.png" alt="recipe thumbnail" />
+  <div class="recipe-details">
+    <div class="recipe-title">${data.title}</div>
+    <div class="recipe-ingredients">${data.ingredients}</div>
+  </div>
+    <div class="recipe-delete">
+    <i class="material-icons" data-id="${id}">delete_outline</i>
+  </div>
+</div>`;
+  recipes.innerHTML += html;
+};
+//remove recipe from doc
+const removeRecipe = (id) => {
+  const recipe = document.querySelector(`div[data-id=${id}]`);
+  recipe.remove();
+};
